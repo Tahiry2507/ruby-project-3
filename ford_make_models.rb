0a248839596718b6b64f4e69116fd79eb0311408
Tahiry2507 <utouch25@gmail.com>
2014-01-08 12:13:05 +0300

    Update ford_make_models.rb
    I have put the questions as comments

--- ford_make_models.rb
+++ ford_make_models.rb
@@ -1,9 +1,14 @@
 # require 'rubygems'
 # require 'fastercsv'
-
 require 'csv'
 
+# 1. Copy your script from Part 2 and name it "ford_make_models.rb"
+# 2. Modify your scripts to store your make/models in a hash
+
 fords = [{ :make => "Ford", :model => "Mustang"}, { :make => "Ford", :model => "Taurus"}]
+
+# 3. Remove any items from the hash that are not a "Ford" make.
+# 4. Create a file called fords.csv and write the data from your Ford only hash
 
 File.write('ford.csv', "Make, Model\n")
 CSV.open("ford.csv", "a") do |csv|
